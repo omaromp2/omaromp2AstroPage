0ec3eccd7af8037a7373024a4834ed0012bcd883
Omar Ponce <omargponce@hotmail.com>
2025-06-19 16:11:57 -0400

    Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://omaromp2.github.io/',
+  site: 'https://omaromp2.github.io/omaromp2AstroPage', // ✅ full project page URL
   base: '/omaromp2AstroPage',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
       serviceEntryPoint: '@astrojs/image/sharp',
       cacheDir: "./.cache/image",
       logLevel: 'debug',
-    }
-  )]
+    }),
+  ]
 });
